8914cc43e184cb4f18f77c448cd29a7def8f23b1
Hoàng Anh Dũng <108377940+DungKM@users.noreply.github.com>
2024-01-09 12:34:10 +0700

    fix Menu sidebar

--- resources/views/layouts/components/sidebar.blade.php
+++ resources/views/layouts/components/sidebar.blade.php
@@ -1,8 +1,12 @@
+@php
+    $currentRoute = \Route::currentRouteName();
+@endphp
+
 <div class="page-sidebar">
     <div class="logo-wrap">
-        <a href="{{route('home')}}">
-            <img src="{{asset('assets/images/logo/13.png')}}" class="img-fluid  for-light" alt="">
-            
+        <a href="{{ route('home') }}">
+            <img src="{{ asset('assets/images/logo/13.png') }}" class="img-fluid  for-light" alt="">
+
         </a>
         <div class="back-btn d-lg-none d-inline-block">
             <i data-feather="chevrons-left"></i>
@@ -12,10 +16,13 @@
         <div class="user-profile">
             <div class="media">
                 <div class="change-pic">
-                    <img src="{{ asset('images/user_avatar/' . Auth::user()->profile_photo_path) }}" class="img-fluid" alt="">
+                    <img src="{{ asset('images/user_avatar/' . Auth::user()->profile_photo_path) }}" class="img-fluid"
+                        alt="">
                 </div>
                 <div class="media-body">
-                    <a href="user-profile.html"><h6>{{ Auth::user()->name }}</h6></a>
+                    <a href="user-profile.html">
+                        <h6>{{ Auth::user()->name }}</h6>
+                    </a>
                     <span class="font-roboto">{{ Auth::user()->email }}</span>
                 </div>
             </div>
@@ -23,36 +30,39 @@
         <div id="mainsidebar">
             <ul class="sidebar-menu custom-scrollbar">
                 <li class="sidebar-item">
-                    <a href="{{route('home')}}" class="sidebar-link only-link active">
-                        <i data-feather="airplay"></i> 
+                    <a href="{{ route('home') }}"
+                        class="sidebar-link only-link {{ $currentRoute == 'home' ? 'active' : '' }}">
+                        <i data-feather="airplay"></i>
                         <span>Trang chủ</span>
-                        
+
                     </a>
                 </li>
                 <li class="sidebar-item">
-                    <a href="javascript:void(0)" class="sidebar-link ">
-                        <i data-feather="home"></i> 
+                    <a href="javascript:void(0)"
+                        class="sidebar-link  {{ $currentRoute == 'loai_phong' || $currentRoute == 'ManagerRoom.index' ? 'active' : '' }}">
+                        <i data-feather="home"></i>
                         <span>Quản Lý Phòng Trọ</span>
                     </a>
-                    <ul class="nav-submenu menu-content">
-                        <li>
-                            <a href="{{route('loai_phong')}}">
+                    <ul class="nav-submenu menu-content ">
+                        <li class="sidebar-item">
+                            <a href="{{ route('loai_phong') }}">
                                 <i data-feather="chevrons-right"></i>
                                 Quản Lý Loại Phòng
                             </a>
                         </li>
                         <li>
-                            <a href="{{route('ManagerRoom.index')}}">
+                            <a href="{{ route('ManagerRoom.index') }}">
                                 <i data-feather="chevrons-right"></i>
-                                Quản lý Phòng 
+                                Quản lý Phòng
                             </a>
                         </li>
 
                     </ul>
                 </li>
                 <li class="sidebar-item">
-                    <a href="javascript:void(0)" class="sidebar-link ">
-                        <i data-feather="inbox"></i> 
+                    <a href="javascript:void(0)"
+                        class="sidebar-link {{ $currentRoute == 'categorynew.index' || $currentRoute == 'news.index' ? 'active' : '' }}">
+                        <i data-feather="inbox"></i>
                         <span>Quản Lý Tin Tức</span>
                     </a>
                     <ul class="nav-submenu menu-content">
@@ -63,7 +73,7 @@
                             </a>
                         </li>
                         <li>
-                            <a href="{{route('news.index')}}">
+                            <a href="{{ route('news.index') }}">
                                 <i data-feather="chevrons-right"></i>
                                 Quản lý Bài Viết
                             </a>
@@ -71,29 +81,31 @@
 
                     </ul>
                 </li>
-                    <li class="sidebar-item">
-                        <a href="{{ route('account.index') }}" class="sidebar-link only-link">
-                            <i data-feather="users"></i>
-                        <span>Quản Lý Tài Khoản</span>
-                        </a>
-                    </li>
                 <li class="sidebar-item">
-                    <a href="javascript:void(0)" class="sidebar-link">
+                    <a href="{{ route('account.index') }}"
+                        class="sidebar-link only-link {{ $currentRoute == 'account.index' ? 'active' : '' }}">
+                        <i data-feather="users"></i>
+                        <span>Quản Lý Tài Khoản</span>
+                    </a>
+                </li>
+                <li class="sidebar-item">
+                    <a href="javascript:void(0)"
+                        class="sidebar-link {{ $currentRoute == 'menus.index' ? 'active' : '' }}">
                         <i data-feather="settings"></i>
                         <span>Cài đặt website</span>
                     </a>
                     <ul class="nav-submenu menu-content">
-                       
+
                         <li>
-                            <a href="{{route('menus.index')}}">
+                            <a href="{{ route('menus.index') }}">
                                 <i data-feather="chevrons-right"></i>
                                 Menu Home
                             </a>
                         </li>
                         <li>
-                            <a href="add-agent.html">
+                            <a href="{{ route('trang_chu') }}">
                                 <i data-feather="chevrons-right"></i>
-                               Thông tin website
+                                Thông tin website
                             </a>
                         </li>
                     </ul>
